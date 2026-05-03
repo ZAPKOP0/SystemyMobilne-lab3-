9242d9141d8c9d136307a1a378d9b3924cb9b1eb
user <pawelkopiczak@gmail.com>
2026-05-03 21:50:48 +0200

    zmiany po lab3

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,90 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, Text, View } from "react-native";
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
+export default function Index() {
   return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
+    <View style={styles.container}>
+      
+      <View style={styles.header}>
+        <Text style={styles.logo}>LogosAplikacji</Text>
+        <Text style={styles.button}>Umów Ponownie</Text>
+      </View>
 
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+      <View style={styles.middle}>
+        <View style={styles.leftBox}>
+           <Text style={styles.titleLBox}>Opinie i oceny</Text>
+           <View style={styles.bottomLBox}>
+             <View style={styles.half}>
+               <Text>zabieg z pracownikiem</Text>
+             </View>
+             <View style={styles.half}>
+               <Text >Umow</Text>
+             </View>
+           </View>
+        </View>
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+        <View style={styles.rightBox}>
+          <Text>zabieg z pracownikiem</Text>
+        </View>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
+    flexDirection: "column",
+    gap:10, 
   },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
+
+  header: {
+    backgroundColor: "#120a57",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center", 
   },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
+  logo: {
+    color: "#ffffff",
+    fontSize: 30,
   },
-  title: {
-    textAlign: 'center',
+  button: {
+    backgroundColor: "#9de6f3",
+    color: "#fd0000",
+    height: 20,
   },
-  code: {
-    textTransform: 'uppercase',
+
+  middle: {
+    flexDirection: "row",
+    height: 200,
+    gap: 10,
   },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+  
+  leftBox: {
+    flex: 3,
+    backgroundColor: "#B0AEC4",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
+  
+  titleLBox: {
+    backgroundColor: "#9ad23a",
+    alignSelf: "flex-end",
+    justifyContent: "space-between",
+  },
+  half: {
+  flex: 1, 
+  },
+
+  bottomLBox: {
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignSelf: "flex-start",
+  width: "100%",
+  },
+
+  rightBox: {
+    flexDirection: "row",
+    backgroundColor: "#D3D3D3",
+    alignItems: "flex-end",
+  },
+
 });
